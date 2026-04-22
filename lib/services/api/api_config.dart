1979423eae34a38261ab265851df4e9d9a220b92
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2026-04-22 14:34:44 +0500

    good job changes

--- lib/services/api/api_config.dart
+++ lib/services/api/api_config.dart
@@ -1,6 +1,6 @@
 // lib/services/api/api_config.dart
 class ApiConfig {
-  static const String baseUrl = 'http://gj-back.checkedout.kz/api';
+  static const String baseUrl = 'https://good-job.kz/api';
   static const int connectionTimeout = 30; // seconds
   static const int receiveTimeout = 30; // seconds
   
